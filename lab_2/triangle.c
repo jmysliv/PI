27a801deeb7d1cb3affe2d5eb51658cf5fee888f
Jakub Myśliwiec <48885911+jmysliv@users.noreply.github.com>
2019-03-25 15:39:08 +0100

    Update triangle.c

--- lab_2/triangle.c
+++ lab_2/triangle.c
@@ -3,16 +3,19 @@
 #include <stdbool.h>
 
 bool is_triangular(int** tab, int n) {
+    if(n==1 && tab[0][0]==0) return true; //corner case
+    if(n<2) return false;   //corner case
     bool flag=true;
+    /*
     for(int i=1; i<n; i++){
         for(int j=0; j<i; j++){
             if(tab[i][j]!=0) flag=false;
         }
         if(!flag) break;
     }
-   if(flag) return true;
-   flag=true;
-   for(int i=1; i<n; i++){
+    if(flag) return true;
+    flag=true; */
+    for(int i=1; i<n; i++){
         for(int j=n-1; j>n-i-1; j--){
             if(tab[i][j]!=0) flag=false;
         }
@@ -26,11 +29,16 @@ bool is_triangular(int** tab, int n) {
 int main() {
     int n;
     scanf("%d", &n);
-    int** tab = malloc(n*n* sizeof(int));
+    int **tab;
+    tab=(int**)malloc(n*sizeof(int *));
+    for(int i=0; i<n; i++)
+    {
+        tab[i]=(int*)malloc(n*sizeof(int));
+    }
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            scanf("%d", tab[i][j]);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &tab[i][j]);
         }
     }
 
